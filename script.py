444653c7fd9f8b6f6e1ac2d30f7cd3979b6e102b
Aidan <aidan@DESKTOP-5HGOIGL>
2024-08-21 09:44:07 -0400

    validate

--- script.py
+++ script.py
@@ -2,11 +2,13 @@ from flexauto import AthenaAutocallable, ABM, GBM, AthenaResult
 import torch
 import torch.nn as nn 
 from torch.nn import Module
+from torch.distributions.log_normal import LogNormal
 import numpy as np
 import os
 import matplotlib.pyplot as plt
 import statistics
 import math
+from tqdm import tqdm
 import scipy
 from scipy.stats import lognorm
 def clean_dir(name):
@@ -34,7 +36,9 @@ def clean_dir(name):
     #     print("epoch: " + str(i) + ", LogL: " + str(obj.item()) + ", mu: " + str(params[0].item()) + ", sigma: " + str(params[1].item()))
     #     hist.append(obj.item())
     #     gd.step()
-        
+
+
+
 
 
 def plot_athena_result(result: AthenaResult, color, term_path) -> None:
@@ -69,7 +73,7 @@ def plot_athena_result(result: AthenaResult, color, term_path) -> None:
     plt.ylabel("spot")
     # plt.savefig('plot.png')
 
-if __name__ == "__main__":
+def run():
     print("Running script")
 
 
@@ -94,7 +98,7 @@ if __name__ == "__main__":
         "observation_dates": [1.0, 2.0, 3.0],
         "coupon_value": 0.05,
         "kill_value": 0.8,
-        "inception_spot":501.0,
+        "inception_spot":50.0,
     }
 
     athena = AthenaAutocallable(*athena_configuration.values())
@@ -110,27 +114,59 @@ if __name__ == "__main__":
         gbm = GBM(*experiment_configuration.values())
         gbm.generate_stock_price()
         result = athena.price_gbm(gbm)
-        if(gbm.getTermIndex() > 900):
-            prices.append(gbm.getTermPath()[-1])    
+        prices.append(gbm.getStockPath()[-1])    
         values.append(result.getPrice())
         results.append(result)
         path_to_terms.append(gbm.getTermPath())
 
-    mu = experiment_configuration["drift"]
-    sigma = experiment_configuration["volatility"]
+    return prices
 
 
-    plt.hist(prices, bins=np.linspace(40, 100, 200), density=True)
-    plt.xscale("log")
-    a, b, c = lognorm.fit(prices)
-    real_pdf = scipy.stats.lognorm.pdf(np.linspace(40, 100, 200), a, b, c)
-    print(a, mu - (sigma ** 2)/2)
-    # exp_pdf = scipy.stats.lognorm.pdf(np.linspace(30, 100, 100), )
-    data = np.sort(prices)
-    plt.plot(np.linspace(40, 100, 200), real_pdf, 'r')
-    # plt.plot(np.linspace(30, 100, 100), exp_pdf, 'g')
-    plt.savefig("dist.png")
-    plt.clf()
+x = torch.tensor(run())
+theta = torch.tensor([0.05, 0.07])
+def obj_function(theta: torch.tensor):
+    mu = theta[0]
+    sig = theta[1]
+    t = torch.tensor(5.0)
+    S0 = torch.tensor(50) 
+    loc = torch.log(S0) + (mu  - 0.5 * torch.pow(sig, 2)) * t
+    scale = sig * torch.pow(t,0.5)
+
+    m = LogNormal(loc, scale)
+    return -m.log_prob(x).sum()
+thetas_in_mu = []
+thetas_in_mu_example = torch.linspace(0.04,0.06,50)
+
+scores = []
+
+for mu_ in thetas_in_mu_example:
+    thetas_in_mu.append(torch.tensor([mu_,theta[1]]))
+    scores.append(obj_function(theta=thetas_in_mu[-1]))
+
+plt.plot(thetas_in_mu_example.flatten(), scores)
+plt.savefig('mle.png')
+theta = torch.tensor([0.05, 0.07])
+
+log_like = obj_function(theta)
+mu = torch.tensor(0.01, requires_grad =True)
+sig = torch.tensor(0.02, requires_grad = True)
+
+
+lr = 1e-2
+optimizer = torch.optim.Adam([mu, sig], lr=lr)
+max_iterations = 10000
+loss = torch.zeros(max_iterations)
+tq_obj = tqdm(range(max_iterations), desc=" Model Training")
+
+# Gradient Iterations
+for it in tq_obj:
+    optimizer.zero_grad()
+    cost = obj_function([mu, sig])
+    cost.backward()
+    optimizer.step()
+    loss[it] = cost
+    tqdm.write(f"Loss {loss[it]}  λ: {lr} mu: {mu}, sig: {sig}")  
+
     # plt.figure(figsize=(16,12))
 
     # for result, term_path in zip(results, path_to_terms): 
